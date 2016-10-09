4447e22804c29e1a19d65300634bdf21b95c2e1a
amin abedi <amin.abedi.1368@gmail.com>
2016-10-09 17:07:03 +0330

    server fix

--- app/client.js
+++ app/client.js
@@ -3,8 +3,8 @@ var jayson = require('jayson');
 var client = jayson.client.http({
   port: 3000
 });
-var post='سلام بر همه'
-client.request('categorize', [post], function(err, error, result) {
+var post='spam\t در این زمانه پد \n\n مبیابرتد'
+client.request('categorize', [post,,,true], function(err, error, result) {
   if(err) throw err;
   console.log(result);
 });
--- app/server.js
+++ app/server.js
@@ -14,16 +14,14 @@ var learnOtherCat;
 var tempfilter = bayes()
 var mynet = Spamist()
 var enableloadfromjson= false
-var t = 65.5
-
+var t = 60
+var do_clean= false
+var revivedClassifier
+var filter
+var stateJson
 var lPhase = false
-
-
-if (lPhase)
-{
-	var messages = mynet.readtext('joke')
-
-	var cleaning_options = {
+var fs = require('fs');
+var cleaning_options = {
 	"need_histo": true,
 	"need_neutral": true,
 	"need_length": true,
@@ -31,59 +29,75 @@ if (lPhase)
 	"neutral_difference": 0.05
 	}
 
-	var options = {
-	"calc_error": {
-	  "do_calc": true,
-	  "iteration": 10,
-	  "train_ratio": 0.8,
-	  "threshold": t
-	},
-	"cleaning" : {
-	  "do_clean": true,
-	  "cleaning_options": cleaning_options
+var options = {
+		"calc_error": {
+		  "do_calc": true,
+		  "iteration": 10,
+		  "train_ratio": 0.8,
+		  "threshold": t
+		},
+		"cleaning" : {
+		  "do_clean": do_clean,
+		  "cleaning_options": cleaning_options
+		}
 	}
+	if (lPhase)
+	{
+		var messages = mynet.readtext('joke')
+		filter = mynet.train(messages,options)
+		console.log(mynet)
+		stateJson = filter.toJson()
+		revivedClassifier = bayes.fromJson(stateJson)
+		
+	}
+	else{
+		
+		var x = require("./data.json")
+		// load the spamfilter back from its JSON representation.
+		revivedClassifier = bayes.fromJson(x)
 	}
 
-	var filter = mynet.train(messages,options)
-	console.log(mynet)
-
-	var stateJson = filter.toJson()
-}
-
-var x = require("./data.json")
-// load the spamfilter back from its JSON representation.
-var revivedClassifier = bayes.fromJson(x)
-
-
-
-
-
+console.log("start server")
 var server = jayson.server({
   categorize: function(args, callback) {
 	
-	//  console.log(callback.toString())  
+	 //console.log("start server")  
 	
 	post=args[0]
-	console.log(post)
-	learnOnfly = args[2]
-	learnOtherCat = args[3]
-	enableloadfromjson=args[4]
-		
-//		if (learnOnfly != '')
-//		{
-		///////////////////////learn on the fly
-//		var filter = mynet.train(learnOnfly,options)
-//		console.log(mynet)
-//		}
-//		if (learnOtherCat != '')
-//		{
-		////////////////////// learn other catagory
-//			var messages = mynet.readtext(learnOtherCat)
-//			var filter = mynet.train(messages,options)
-//			console.log(mynet)
-//		} 
-		
+	
+	if(args[1]!=null)
+	{
+		console.log(args[1])
+		learnOnfly=args[1]	
+		learning(learnOnfly)
+	}
+	if(args[2]!=null)
+	{
+		if(typeof args[2]== "number")
+		{
+			t=args[2]
+			
+		}
+	}
 	//console.log(post)
+	if(args[3]!=null)
+	{
+		if(typeof args[3]== "boolean")
+		{
+			console.log(args[3])
+			options.cleaning.do_clean=args[3]
+			console.log(options)
+			var messages = mynet.readtext('joke')
+			filter = mynet.train(messages,options)
+			console.log(mynet)
+			stateJson = filter.toJson()		
+			revivedClassifier = bayes.fromJson(stateJson)
+		}
+	}
+	
+		
+	console.log("t",t)
+	console.log(post)
 	//console.log(revivedClassifier)
 	var result = mynet.classify(post,revivedClassifier,t)	
 	console.log(result)
@@ -113,3 +127,14 @@ app.use(basicAuth(function(user, pass){
 }))
 app.use(server.middleware());
 app.listen(3000);
+
+function learning(learnOnfly)
+{
+	var messages = learnOnfly
+	messages = messages.replace(/\n/g, " ");
+    messages = messages.toLowerCase()
+	messages = messages+'\n'
+	var file= "./resource/joke"
+	fs.appendFile(file,messages, function (err) {
+	});
+}
